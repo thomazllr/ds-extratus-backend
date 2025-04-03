56bba20a4dd962860b97080c70c72b3ab2af01cd
thomazllr <guithomaz270@gmail.com>
2025-04-03 11:50:37 -0300

    initial commit

---
+++ src/main/java/com/thomazllr/ds_extratus/DsExtratusApplication.java
@@ -0,0 +1,13 @@
+package com.thomazllr.ds_extratus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsExtratusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsExtratusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomazllr/ds_extratus/DsExtratusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomazllr.ds_extratus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsExtratusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
