020888896dc6a47ee6b7f65b17e63ced35aa23ea
thomazllr <guithomaz270@gmail.com>
2025-04-03 12:16:24 -0300

    feat(model): modelando entidades

---
+++ src/main/java/com/thomazllr/ds_extratus/model/Cliente.java
@@ -0,0 +1,37 @@
+package com.thomazllr.ds_extratus.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String cpf;
+
+    private LocalDate dataNascimento;
+
+    private String telefone;
+
+    private String email;
+
+    private String endereco;
+
+    @ManyToMany
+    private List<Profissao> profissoes;
+}
